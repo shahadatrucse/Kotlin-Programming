00e3df7e440bbedaca373aaff90d50c3c35dad9b
shahadatrucse <shahadat.rucse@gmail.com>
2021-12-14 20:33:51 +0600

    I/O in Kotlin

---
+++ DiceRoller.kt
@@ -0,0 +1,20 @@
+package `Kotlin-Programming`
+
+import kotlin.random.Random
+
+
+
+fun main()
+{
+    val diceRange : IntRange = 1 .. 6
+    val randomNumber = diceRange.random()
+    //println("Your random variable is : ${randomNumber}")
+    //println("Print output from Dice class.")
+    val myFirstDice = Dice(5)
+    println("My ${myFirstDice.numSides} sided dice rolled  ${myFirstDice.roll()}")
+    println("Update.....")
+    val mySecondDice = Dice(100)
+    println("My ${mySecondDice.numSides} sided dice rolled  ${mySecondDice.roll()}")
+
+
+}
